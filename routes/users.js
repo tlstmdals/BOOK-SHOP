d33c513ecf324132d3aa13b07e65706b8c6c9c85
SSM <ssm102030@naver.com>
2024-12-13 00:16:48 +0900

    second commit

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('../mariadb');
+const {
+    join,
+    login,
+    passwordResetRequest,
+    passwordReset
+ } = require('../controller/UserController');
+
+const { Result } = require('express-validator');
 router.use(express.json());
 
-router.post('/join',(req,res)=>{
-    
-});
-router.post('/login');
-router.post('/reset');
-router.put('/reset');
+router.post('/join', join);
+router.post('/login', login);
+router.post('/reset', passwordResetRequest);
+router.put('/reset', passwordReset);
 
 module.exports = router;
